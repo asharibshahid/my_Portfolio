053a6878a049398ff62cc35454e712e7cb65dd3b
ASHARIB-SHEIKH👩🏻‍💻 <160227183+asharibshahid@users.noreply.github.com>
2025-07-09 02:13:06 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 
-import Portfolio from "./navbar";
-import Page from "./projects/page";
-import ContactPage from "./contact/page";
+
 import About from "./about/page";
 export default function Home() {
   return (
    <> 
   
-   <Portfolio />
-   <Page />
+   
    <About />
-   <ContactPage />
+   
    </>
   );
 }
